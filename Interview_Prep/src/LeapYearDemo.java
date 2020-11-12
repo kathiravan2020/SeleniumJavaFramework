21dfd750edbfcdf54e884bf2ea21623361b60603
Kathiravan.Ramachand <Kathiravan.Ramachand@MOC-L-RAMACHAND.na.sensus.net>
2020-11-11 22:39:50 -0500

    Adding LeapYearDemo into Github repositary

---
+++ Interview_Prep/src/LeapYearDemo.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+public class LeapYearDemo {
+
+	public static void main(String[] args) {
+		// Leap year demo
+		
+		//User need to enter input year
+		
+		Scanner scr = new Scanner(System.in);
+		int year;
+		System.out.print("Enter a year  = ");
+		year=scr.nextInt();
+		scr.close();
+		
+		if (year % 4 == 0 && year % 100 !=0){
+			
+			System.out.println(year + "  Is a Leap Year" + "  ");
+		}
+		else if (year % 400 == 0){
+			
+			System.out.println(year + "   Is a leap year" + "   ");
+		}
+		
+	else {
+			System.out.println(year + "   Is not a leap year" + "   ");
+		}
+	}
+
+}
